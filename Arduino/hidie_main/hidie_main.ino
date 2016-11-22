1aef7121fab9688f1898b3d9898e2cbe000cecf9
guuse <ekkelenkampguus@gmail.com>
2016-11-22 14:53:11 +0100

    added missing files :)

--- Arduino/hidie_main/hidie_main.ino
+++ Arduino/hidie_main/hidie_main.ino
@@ -1,9 +1,10 @@
-void setup() {
-  // put your setup code here, to run once:
+float celsius = 0;
 
+void setup(){
+   Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  celsius = readTemp();
+  delay(1000);
 }
