8b7b7c2cdb178d111b052ba4c8a71697780e3435
HLA-systeem <HLA-systeem@users.noreply.github.com>
2016-12-08 14:54:42 +0100

    getting started with the lcd
    I put all the need-to-know in comments.

--- Arduino/hidie_main/display.ino
+++ Arduino/hidie_main/display.ino
@@ -1,3 +1,31 @@
+//De lcd moet compatible zijn met de  Hitachi HD44780 driver 
+//1x16 LCD display will have one row of sixteen characters
+//4x20 LCD display will have four rows with twenty characters in each. 
+//gnd (first pin from left to right)
+//VCC = powerPin (second pin is 5 V)
+//Vo = contrastPin (3e)
+//RS = commandOrCharacterspin (4e)
+//R/W = readOrWritePin (5e)
+//enablePin(6e)
+//DO-D7(7e tot 14e)
+//anode = lightPin (15e)
+//cathode = lightPin (16e)
+
+#include <LiquidCrystal.h>
+
+const rsPin = 1; 
+const enablePin = 2;
+const d4 = 4;
+const d5 = 5;
+const d6 = 6;
+const d7 = 7;
+
+LiguidCrystal lcd(rsPin,enablePin,d4,d5,d6,d7);
+
+void lcdSetup(){
+  lcd.begin(16,2);//dimesies van de lcd
+  }
+
 void displayData(float celius,float weight,float waterlevel){
   
   }
--- Arduino/hidie_main/hidie_main.ino
+++ Arduino/hidie_main/hidie_main.ino
@@ -3,11 +3,12 @@ float celsius = 0;
 
 void setup(){
    Serial.begin(9600);
+   lcdSetup();
 }
 
 void loop(){
   celsius = readTemp();
-  //Serial.println(celsius);
-  //Serial.print(' Celsius');
+  Serial.println(celsius);
+  Serial.print(' Celsius');
   delay(1000);
 }
